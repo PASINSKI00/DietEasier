3ecf8747f62288b76a27f2f53f5b7a87ccd4dc6b
Krystian Pasinski <krystian.pasinski00@gmail.com>
2021-11-20 04:49:34 +0100

    (#9)

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+function openLogin() {
+    document.getElementById("log-in").style.display = "flex";    
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("log-in").style.opacity = '1'; 
+
+}
+
+function openSignUp() {
+    document.getElementById("sign-up").style.display = "flex";    
+    document.getElementById("overlay").style.display = "block";   
+}
+
+function hideLogins() {
+    document.getElementById("log-in").style.display = "none";    
+    document.getElementById("sign-up").style.display = "none";    
+    document.getElementById("overlay").style.display = "none";   
+}
