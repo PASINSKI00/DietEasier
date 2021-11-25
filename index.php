1b38839af2bf7a3e7bac46bec709d120a4fed5a1
Krystian Pasinski <krystian.pasinski00@gmail.com>
2021-11-25 12:28:18 +0100

    testing

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hi there 👋';
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+   ];
+   
+   $ary = array("1"=>'One','Two',"3"=>3);
+   $a = '0'; $b = count($ary);
+   while ($a <= $b) {
+    $pr = $ary[$a];
+    print "$pr<br>";
+    $a++;
+   } 
