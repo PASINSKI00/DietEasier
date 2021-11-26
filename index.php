5f3f54c18205a2e03469d1d9c0e113df2e7ef37f
Krystian Pasinski <krystian.pasinski00@gmail.com>
2021-11-26 20:46:34 +0100

    (#11)

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 
-$array = [
-    "foo" => "bar",
-    "bar" => "foo",
-   ];
-   
-   $ary = array("1"=>'One','Two',"3"=>3);
-   $a = '0'; $b = count($ary);
-   while ($a <= $b) {
-    $pr = $ary[$a];
-    print "$pr<br>";
-    $a++;
-   } 
+require "Router.php";
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('home', 'DefaultController');
+Router::get('chooseMeals', 'DefaultController');
+Router::run($path);
+
+
+?>
